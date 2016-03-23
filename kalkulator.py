9d834b8fcc4f3cf96c9d450a246d03f0ce9fa994
Student UEP <student@student.ue.poznan.pl>
2016-03-23 12:57:29 +0100

    dodalem funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -2,6 +2,10 @@ def dodawanie():
 	a = int(input("Podaj a"))
 	b = int(input("Podaj b"))
 	print(a+b)
-	
+
+def get_info():
+	return "To jest program kalkulator stworzony przez Jacka"
+
+print(get_info())
 dodawanie()
 input()
